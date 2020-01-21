8057a2c3ecc213da8c79ac62b8f1bc3a043c15cb
unknown <gikkhelp@gmail.com>
2020-01-21 23:05:37 +0100

    first azure project

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,8 @@
-package com.sanumala;
 public class HelloWorld
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello Simplon @ Azure Project");   
+         System.out.println("Hello MyName @ Azure Project");   
       }
 } 
 
